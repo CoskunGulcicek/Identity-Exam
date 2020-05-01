5e24f2e244bc6c287622d655d4c915a2e0c8affd
CoskunGulcicek <63802797+CoskunGulcicek@users.noreply.github.com>
2020-05-01 03:39:32 +0300

    Add project files.

---
+++ Identity/Contexts/UdemyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Contexts
+{
+    public class UdemyContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=UdemyIdentity ;Trusted_Connection=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
